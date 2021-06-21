b2e0880a8c5042f4ba9a4e16f49aa30c93fad596
Kevin Alves <kevinalves@hotmail.fr>
2021-06-21 09:42:40 +0200

    Maj ZOO 21/06

--- Code/main.cpp
+++ Code/main.cpp
@@ -1,7 +1,38 @@
-#include "./includes/zoo.hpp"
+/*
+Simulateur de Zoo
+
+Ce projet consiste à créer un simulateur permettant la gestion d'un zoo en tour par tour. Chaque tour représente 1 mois.
+
+Notre simulateur doit mettre en place : 
+    - Un scénario avec : 2 couples de tigre, 2 couples d'aigle, 1 coq, 10 poules sur "10 ANS".
+    - Des Classes (Animaux, tigre, poules, zoo etc..)
+    - La gestion des 4 actions :
+        - Achat/Vente animal ;
+        - Achat nourriture ;
+        - Achat/Vente Habitat;
+        - Tour suivant;
+    - La gestion des 10 événements :
+        - Grossesses ;
+        - Naissance ;
+        - Mort Infantile ;
+        - Fin de vie ;
+        - Surpopulation ;
+        - Maladie;
+        Execptionnels par mois :
+            - Incendie ;
+            - Vol ;
+            - Nuisibles ; 
+            - Viande avariée
+*/
+
+#include "./includes/zoo.h"
+#include <string>
+#include <iostream>
+#include <vector>
 
 
 
 int main() {
-    DisplayOut();
+    // DisplayOut();
+    return 0;
 }
