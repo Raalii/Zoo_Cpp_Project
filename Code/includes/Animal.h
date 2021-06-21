6c365dc5d76fba9b229d984f3c182cbfbd45421c
Kevin Alves <kevinalves@hotmail.fr>
2021-06-21 10:23:55 +0200

    Maj ZOO 21/06

--- Code/includes/Animal.h
+++ Code/includes/Animal.h
@@ -1,25 +1,31 @@
+#ifndef ANIMAL_H
+#define ANIMAL_H
+
 #include <iostream>
 #include <string>
 
 using namespace std;
 
-class Animal
-{
-protected:
-    bool m_Gender; // True for male, false for female
-    bool m_Malade; 
-    int m_Age;
-    string m_Type_Of_Food; // Graine of viande
-    float m_Quatity; // Quantity of consommation food
-public:
-    Animal(/* args */);
-    ~Animal();
+class Animal {
+    protected:
+        bool m_Gender; // True for male, false for female
+        bool m_Malade; 
+        int m_Age;
+        string m_Type_Of_Food; // Graine of viande
+        float m_Quatity; // Quantity of consommation food
+    public:
+        Animal(/* args */);
+        ~Animal();
 };
 
-Animal::Animal(/* args */)
-{
+Animal::Animal(/* args */){
+
 }
 
-Animal::~Animal()
-{
+Animal::~Animal(){
+    
 }
+
+
+
+#endif
