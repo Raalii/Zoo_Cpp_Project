65aeb60b188501b08945e453f726bbe72f8f9dca
Rayyouu <rayane.ainseba@ynov.com>
2021-06-22 11:17:27 +0200

    Maj habitat.h 22/06/21

--- Code/includes/budget.h
+++ Code/includes/budget.h
@@ -14,13 +14,18 @@ class Budget {
         void Add_Money_By_Subvention(int month, int Nbr_Tiger, int Nbr_Eagle);
         void Money_Incomme_By_Visitors(int month, int Nbr_Tiger, int Nbr_Poule, int Nbr_Eagle);
         float Get_Capital();
-        void By_Habitat(int price);
+        void Buy_Habitat(int price);
+        void Sell_Habitat(int price);
 };
 
 
 
-void Budget::By_Habitat(int price) {
+void Budget::Buy_Habitat(int price) {
     m_Capital -= price;
-} ;
+};
+
+void Budget::Sell_Habitat(int price) {
+    m_Capital += price;
+};
 
 #endif
--- Code/includes/habitat.h
+++ Code/includes/habitat.h
@@ -22,6 +22,7 @@ public:
     habitat();
     ~habitat();
     float buy_habitat_for_animals(Budget Current_Budget);
+    float sell_habitat_animals(Budget Current_Budget);
 };
 
 
@@ -60,8 +61,40 @@ float habitat::buy_habitat_for_animals(Budget Current_Budget)
     }
 }
 
-float habitat::sell_habitat_animal() {
-    
+float habitat::sell_habitat_animals(Budget Current_Budget) {
+    int choice;
+
+    do
+    {
+        std::cout << "Souhaitez-vous vendre un habitat ? " << std::endl;
+        std::cout << "Si oui, lequels ? 1 habitat tigre, 2 habitat aigle, 3 habitat poules." << std::endl;
+        std::cin >> choice;
+    } while (choice != 1 && choice != 2 && choice != 3);
+
+    switch (choice)
+    {
+    case 1: /*Habitat tigre*/
+        // alors le nombre habitat tigre -1
+        m_Habitat.h_tigre -= 1;
+        Current_Budget.Sell_Habitat(500);
+        nbr_of_habitats -=1;
+        break;
+
+    case 2: /*Habitat aigle*/
+        // alors le nombre habitat aigle +1
+        m_Habitat.h_aigle -= 1;
+        Current_Budget.Sell_Habitat(500);
+        nbr_of_habitats -=1;
+        break;
+    case 3: /*Habitat poule*/
+        // alors le nombre habitat poule +1
+        m_Habitat.h_poule -= 1;
+        Current_Budget.Sell_Habitat(50);
+        nbr_of_habitats -=1;
+        break;
+    default:
+        break;
+    }
 }
 
 
